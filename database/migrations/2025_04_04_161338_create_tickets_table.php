1016bbce8125fd5c022c42a0292dfbab1e9e7c1d
ayoubbr <ayoub1benmansour@gmail.com>
2025-04-04 16:31:35 +0000

    migration, model and controller for role, user, stadium, team, catgory, game and ticket

--- database/migrations/2025_04_04_161338_create_tickets_table.php
+++ database/migrations/2025_04_04_161338_create_tickets_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('game_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('price');
+            $table->integer('place_number');
+            $table->string('status')->default('pending');
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
