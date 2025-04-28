9133f512988087cdc52bf2bbbd936d5c427c8424
ayoubbr <ayoub1benmansour@gmail.com>
2025-04-28 14:54:52 +0100

    mark all notifications as read

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    //
+
+
+    public function markAllAsRead()
+    {
+        $userId = auth()->user()->id;
+
+        $notifications = Notification::where('user_id', $userId)->where('status', 'unread')->get();
+
+        if ($notifications->count() < 1) {
+            return redirect()->back()->with('error', 'Something went wrong!');
+        } else {
+            foreach ($notifications as $notification) {
+                $notification->status = 'read';
+                $notification->save();
+            }
+            return redirect()->back()->with('success', 'All Notifications set to read');
+        }
+    }
 }
